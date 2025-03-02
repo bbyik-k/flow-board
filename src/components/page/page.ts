627329f5a39425ebf01b5644cfe4a7933b2bb57a
bbyik-k <kby00517@naver.com>
2025-03-02 23:38:52 +0900

    refactor: add base component class & interface

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,12 +1,8 @@
-export class PageComponent {
-  private element: HTMLUListElement;
-  constructor() {
-    this.element = document.createElement('ul');
-    this.element.classList.add('page');
-    this.element.textContent = 'This is PageCompnents';
-  }
+import { BaseComponent } from '../component.js';
 
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-    parent.insertAdjacentElement(position, this.element);
+export class PageComponent extends BaseComponent<HTMLUListElement> {
+  constructor() {
+    super('<ul>This is Page Components</ul>');
+    this.element.classList.add('page');
   }
 }
