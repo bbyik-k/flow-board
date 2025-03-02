a41d2faf041af2f44fb9357f1985b73f91fdf990
bbyik-k <kby00517@naver.com>
2025-03-03 07:12:34 +0900

    feat: add Note component

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { ImageComponent } from './components/item/image.js';
+import { NoteComponent } from './components/item/note.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -8,8 +9,10 @@ class App {
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-
     image.attachTo(appRoot, 'beforeend');
+
+    const note = new NoteComponent('Note Title', 'Note body text hello hello');
+    note.attachTo(appRoot, 'beforeend');
   }
 }
 
