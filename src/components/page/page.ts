599d433c66cbbf2b05d5f2d5fb149a87a623bf8b
bbyik-k <kby00517@naver.com>
2025-03-05 17:42:11 +0900

    feat: Remove component

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,6 +4,7 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
+type OnCloseListener = () => void;
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
   constructor() {
     super('<ul></ul>');
@@ -14,10 +15,14 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
 
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
             <section class="page-item__body">
@@ -26,9 +31,19 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
               </div>
             </section>
           </li>`);
+
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
+
   addChild(child: Component) {
     const container = this.element.querySelector('.page-item__body')! as HTMLElement;
     child.attachTo(container);
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
